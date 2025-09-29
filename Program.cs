81427b285677bc7e59e3a5ab20725829ab1d2236
Jaydon Rodrigues <Rodriguesjaydon18@outlook.com>
2025-09-29 15:19:15 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Rodrigues.WordCount.App15
@@ -11,15 +11,15 @@ namespace Rodrigues.WordCount.App15
             Console.WriteLine("Enter a line of text, and I’ll count the frequency of each word:");
             string input = Console.ReadLine();
 
-            
+           
             CleanWords cleaner = new CleanWords();
             string[] words = cleaner.ProcessInput(input);
 
-           
+            
             CountWords counter = new CountWords();
             Dictionary<string, int> results = counter.Count(words);
 
-            
+           
             Console.WriteLine("\n=== Word Frequencies ===");
             foreach (var pair in results)
             {
